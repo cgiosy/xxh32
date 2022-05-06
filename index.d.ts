abcd640dc876ddea78fb123f9d1b4981c2adc169
cgiosy <cgiosy@kakao.com>
2022-05-06 23:35:18 +0900

    Restructure

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-export default function xxh32(buf: Uint8Array, seed?: number): number;
--- stream.d.ts
+++
@@ -1,6 +0,0 @@
-type XXH32Stream = {
-    update: (buf: Uint8Array) => XXH32Stream;
-    digest: () => number;
-};
-
-export default function xxh32s(seed?: number): XXH32Stream;
